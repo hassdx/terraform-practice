880bd9e2b7b0c8b3ca8ca91b74d945ed93fb1f01
hassan-elhouari <elhouhass@gmail.com>
2026-05-24 22:52:44 +0000

    run the terraform fmt coomad before push

--- base/network_security.tf
+++ base/network_security.tf
@@ -40,9 +40,9 @@ resource "aws_security_group" "http-inbound" {
   vpc_id      = aws_vpc.HASSAN-ELHOUARI-01-vpc.id
 
   ingress {
-    from_port      = 80
-    to_port        = 80
-    protocol       = "tcp"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
     security_groups = [aws_security_group.lb-http-inbound.id]
   }
   tags = {
