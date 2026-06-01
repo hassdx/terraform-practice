9987e193fabc702a90ea6c835e23881dbd846dcb
hassan-elhouari <elhouhass@gmail.com>
2026-06-01 17:19:35 +0000

    task 1 fixied

--- base/outputs.tf
+++ base/outputs.tf
@@ -1,24 +1,24 @@
 output "vpc_id" {
-  value = aws_vpc.cmtr-3v98t79h-01-vp.id
+  value = aws_vpc.main.id
 }
 
 output "subnet_public_a_id" {
-  value = aws_subnet.cmtr-3v98t79h-01-subnet-public-a.id
+  value = aws_subnet.public_a.id
 }
 
 output "subnet_public_b_id" {
-  value = aws_subnet.cmtr-3v98t79h-01-subnet-public-b.id
+  value = aws_subnet.public_b.id
 }
 
 output "subnet_public_c_id" {
-  value = aws_subnet.cmtr-3v98t79h-01-subnet-public-c.id
+  value = aws_subnet.public_c.id
 }
 
 output "igw_id" {
-  value = aws_internet_gateway.cmtr-3v98t79h-01-igw.id
+  value = aws_internet_gateway.igw.id
 }
 
 output "rt_id" {
-  value = aws_route_table.cmtr-3v98t79h-01-rt.id
+  value = aws_route_table.rt.id
 }
 
